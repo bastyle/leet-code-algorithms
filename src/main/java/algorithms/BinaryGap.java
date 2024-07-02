2771310121c1ef8178b4ca7caf3d5f432a9cc250
Bastian Bastias Sanchez <bastian.bastias@gmail.com>
2024-07-01 23:37:53 -0400

    added binary gap description

--- src/main/java/algorithms/BinaryGap.java
+++ src/main/java/algorithms/BinaryGap.java
@@ -2,23 +2,55 @@ package algorithms;
 
 public class BinaryGap {
 
+	/**
+	 * 
+	 * 
+	 * Given a positive integer n, find and return the longest distance between any
+	 * two adjacent 1's in the binary representation of n. If there are no two
+	 * adjacent 1's, return 0.
+	 * 
+	 * Two 1's are adjacent if there are only 0's separating them (possibly no 0's).
+	 * The distance between two 1's is the absolute difference between their bit
+	 * positions. For example, the two 1's in "1001" have a distance of 3.
+	 * 
+	 * 
+	 * 
+	 * Example 1:
+	 * 
+	 * Input: n = 22 Output: 2 Explanation: 22 in binary is "10110". The first
+	 * adjacent pair of 1's is "10110" with a distance of 2. The second adjacent
+	 * pair of 1's is "10110" with a distance of 1. The answer is the largest of
+	 * these two distances, which is 2. Note that "10110" is not a valid pair since
+	 * there is a 1 separating the two 1's underlined. Example 2:
+	 * 
+	 * Input: n = 8 Output: 0 Explanation: 8 in binary is "1000". There are not any
+	 * adjacent pairs of 1's in the binary representation of 8, so we return 0.
+	 * Example 3:
+	 * 
+	 * Input: n = 5 Output: 2 Explanation: 5 in binary is "101".
+	 * 
+	 * 
+	 * Constraints:
+	 * 
+	 * 1 <= n <= 109
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int number = 8;
 		final String binaryNumber = Integer.toBinaryString(number);
 		System.out.println(binaryNumber);
-		int longestGap = 0, auxGap=1;
-		
-		for (int i = 1; i < binaryNumber.length(); i++) {			
+		int longestGap = 0, auxGap = 1;
+
+		for (int i = 1; i < binaryNumber.length(); i++) {
 			if (binaryNumber.charAt(i) == '0') {
 				auxGap++;
 				continue;
 			} else {
-				longestGap = (auxGap>longestGap?auxGap:longestGap);
-				auxGap=1;//reset
+				longestGap = (auxGap > longestGap ? auxGap : longestGap);
+				auxGap = 1;// reset
 			}
 		}
-		
+
 		System.out.println(longestGap);
 	}
 
